5c3d2b625e4356c2ba3931e1cbd69d3e78d77c3c
Leo Settele <leo.settele@gmail.com>
2015-12-02 01:27:33 +0100

    introduce content-script

---
+++ reSoup.js
@@ -0,0 +1,10 @@
+console.debug("NOW IS", new Date());
+// No tabs or host permissions needed!
+document.body.style.backgroundColor="red";
+
+/*
+$('#user-container').each(function (elem) {
+    console.debug("frrrt");
+    $(elem).css('border-width', 'thick');
+})
+ */
