e66ed8b3ef028782112e953c65f7cf8eafab0455
Leo Settele <leo.settele@gmail.com>
2015-12-02 02:46:27 +0100

    map of posts, onScroll handling

--- reSoup.js
+++ reSoup.js
@@ -1,3 +1,34 @@
-$('.user_container').each(function (_,elem) {
-    $(elem).css("border", "3px solid red" );
+var index = 0;
+var wH = $(window).height();
+
+var onScreen = function (wS, post) {
+    return wS > (post.top + post.height - wH)
+};
+
+var posts = $('.post').map(function (idx, elem) {
+    var jqe = $(elem);
+    var wS = $(this).scrollTop();
+    var post = {
+        top: jqe.offset().top,
+        height: jqe.outerHeight(),
+        elem: jqe
+    };
+    if (onScreen(wS, post)) {
+        index = idx;
+        console.debug("setting scroll index to", idx);
+    }
+    return post;
+});
+
+posts.sort(function (a, b) {
+    return a.top - b.top;
+});
+
+$(window).scroll(function () {
+    var wS = $(this).scrollTop();
+    var p = posts[index];
+    if (onScreen(wS, p)) {
+        //FIXME find the correct index of the current post
+        console.debug("scrolled past", p.elem);
+    }
 });
