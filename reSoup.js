49943ee719363137980d210ce4573ba3527a06d6
Leo Settele <leo.settele@gmail.com>
2015-12-27 21:34:31 +0100

    implement binary search to detect the top post

--- reSoup.js
+++ reSoup.js
@@ -1,22 +1,11 @@
-var index = 0;
-var wH = $(window).height();
-
-var onScreen = function (wS, post) {
-    return wS > (post.top + post.height - wH)
-};
-
 var posts = $('.post').map(function (idx, elem) {
     var jqe = $(elem);
     var wS = $(this).scrollTop();
     var post = {
         top: jqe.offset().top,
         height: jqe.outerHeight(),
-        elem: jqe
+        elemId: jqe.attr('id')
     };
-    if (onScreen(wS, post)) {
-        index = idx;
-        console.debug("setting scroll index to", idx);
-    }
     return post;
 });
 
@@ -24,11 +13,22 @@ posts.sort(function (a, b) {
     return a.top - b.top;
 });
 
+var findPost = function (scrollTop, recPosts) {
+    var length = recPosts.length;
+    if (length === 1) {
+        return recPosts[0];
+    }
+    var middleIdx = Math.floor(length / 2);
+    var middle = recPosts[middleIdx];
+    if (middle.top < scrollTop) {
+        return findPost(scrollTop, recPosts.slice(middleIdx, length));
+    } else {
+        return findPost(scrollTop, recPosts.slice(0, middleIdx));
+    }
+};
+
 $(window).scroll(function () {
     var wS = $(this).scrollTop();
-    var p = posts[index];
-    if (onScreen(wS, p)) {
-        //FIXME find the correct index of the current post
-        console.debug("scrolled past", p.elem);
-    }
+    var post = findPost(wS, posts);
+    console.debug("scrolled", wS, post);
 });
